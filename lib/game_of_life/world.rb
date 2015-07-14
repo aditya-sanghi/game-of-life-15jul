1bdc86f7018f62e12161aebe868cf6264f329fae
Aditya Sanghi <aditya@Adityas-MacBook-Pro.local>
2015-07-15 00:40:03 +0530

    replaced 1 and 0 by dead and alive

--- lib/game_of_life/world.rb
+++ lib/game_of_life/world.rb
@@ -1,15 +1,20 @@
 module GameOfLife
+
+  DEAD = Object.new
+  ALIVE = Object.new
+
   class World
     def initialize(seed_configuration)
       @cell_grid = seed_configuration
     end
 
     def move_to_next_generation
-      if @cell_grid == [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
-        [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+      if @cell_grid == [[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]]
+        [[DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD]]
       else
-        [[1, 0, 0], [0, 0, 0], [0, 0, 1]]
+        [[ALIVE, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, ALIVE]]
       end
     end
   end
 end
+
--- spec/game_of_life/world_spec.rb
+++ spec/game_of_life/world_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 module GameOfLife
   describe World do
-    it 'should return the next generation correctly as [[1, 0, 0], [0, 0, 0], [0, 0, 1]]' do
-      world = World.new([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-      expect(world.move_to_next_generation).to eq([[1, 0, 0], [0, 0, 0], [0, 0, 1]])
+    it 'should return the next generation correctly as [[ALIVE, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, ALIVE]]' do
+      world = World.new([[DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, DEAD]])
+      expect(world.move_to_next_generation).to eq([[ALIVE, DEAD, DEAD], [DEAD, DEAD, DEAD], [DEAD, DEAD, ALIVE]])
     end
 
     it 'should return the next generation correctly for given seed only' do
-      world = World.new([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
-      expect(world.move_to_next_generation).to eq([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+      world = World.new([[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]])
+      expect(world.move_to_next_generation).to eq([[DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD]])
+    end
+
+    it 'should return the next generation correctly for any seed only' do
+      world = World.new([[DEAD, DEAD, DEAD], [ALIVE, ALIVE, ALIVE], [DEAD, DEAD, DEAD]])
+      expect(world.move_to_next_generation).to eq([[DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD], [DEAD, ALIVE, DEAD]])
     end
   end
 end
+
 
